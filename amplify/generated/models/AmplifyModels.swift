ab6515003aab446ea07056e6303ddb9d2fba998c
HiveYuan <45333458+HiveYuan@users.noreply.github.com>
2021-10-29 18:11:28 -0500

    add data model: post
    add post model and test API

--- amplify/generated/models/AmplifyModels.swift
+++ amplify/generated/models/AmplifyModels.swift
@@ -5,9 +5,9 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "62b05af285791661ed844e0e1fc7c1f8"
+  public let version: String = "ac3e752a9cb7bf23d71ce0b70381c784"
   
   public func registerModels(registry: ModelRegistry.Type) {
-    ModelRegistry.register(modelType: AABBCC.self)
+    ModelRegistry.register(modelType: Post.self)
   }
 }
---
+++ amplify/generated/models/Post+Schema.swift
@@ -0,0 +1,37 @@
+// swiftlint:disable all
+import Amplify
+import Foundation
+
+extension Post {
+  // MARK: - CodingKeys 
+   public enum CodingKeys: String, ModelKey {
+    case id
+    case author
+    case departureTime
+    case source
+    case destination
+    case transportation
+    case description
+    case maxMembers
+  }
+  
+  public static let keys = CodingKeys.self
+  //  MARK: - ModelSchema 
+  
+  public static let schema = defineSchema { model in
+    let post = Post.keys
+    
+    model.pluralName = "Posts"
+    
+    model.fields(
+      .id(),
+      .field(post.author, is: .optional, ofType: .string),
+      .field(post.departureTime, is: .optional, ofType: .time),
+      .field(post.source, is: .optional, ofType: .string),
+      .field(post.destination, is: .optional, ofType: .string),
+      .field(post.transportation, is: .optional, ofType: .string),
+      .field(post.description, is: .optional, ofType: .string),
+      .field(post.maxMembers, is: .optional, ofType: .int)
+    )
+    }
+}
