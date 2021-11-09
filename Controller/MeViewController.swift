b61ab9fc1591dbf9465b62eff94ffa883e5cd7ea
hebeilidu <dli1@wustl.edu>
2021-11-08 22:25:47 -0600

    Modify the MeViewController: 1.User can take photo as their profile image; 2.User can select photo from the photo library; 3.User can save current image to their photo library

--- Controller/MeViewController.swift
+++ Controller/MeViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 import Amplify
 
-class MeViewController: UIViewController {
+class MeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var takePicButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var getFromLibraryButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
     
     override func viewDidLoad() {
@@ -17,10 +21,15 @@ class MeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        logoutButton.layer.borderColor = UIColor.systemBlue.cgColor
         logoutButton.layer.borderWidth = 1.5
         logoutButton.layer.cornerRadius = 10
-        //？？？？？？
+        takePicButton.layer.borderWidth = 1.5
+        takePicButton.layer.cornerRadius = 10
+        saveButton.layer.borderWidth = 1.5
+        saveButton.layer.cornerRadius = 10
+        getFromLibraryButton.layer.borderWidth = 1.5
+        getFromLibraryButton.layer.cornerRadius = 10
+        
     }
     
     func signOutLocally() {
@@ -42,14 +51,62 @@ class MeViewController: UIViewController {
             }
         }
     }
-    
-    @IBAction func LogOutPressed(_ sender: Any) {
-        signOutLocally()
-    }
 
     func alert(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+       guard let selectedImage = info[.originalImage] as? UIImage else {
+        fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        userImage.image = selectedImage
+        
+        dismiss(animated:true, completion: nil)
+
+            
+    }
+    
+    @IBAction func takePic(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func accessLibrary(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func SavePressed(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(userImage.image!,self,#selector(MeViewController.image(image:didFinishSavingWithError:contextInfo:)),nil)
+    }
+    @objc func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+    
+        if error == nil {
+           let ac = UIAlertController(title: "Saved!", message: "image has been saved to your photos." , preferredStyle: .alert)
+           ac.addAction(UIAlertAction(title:"OK", style: .default, handler:nil))
+           present(ac, animated:true, completion:nil)
+        } else{
+           let ac = UIAlertController(title:"Save error", message: error?.localizedDescription,preferredStyle: .alert)
+           ac.addAction(UIAlertAction(title:"OK", style: .default, handler:nil))
+           present(ac, animated:true, completion:nil)
+        }
+
+        
+    }
+    
+    @IBAction func LogOutPressed(_ sender: Any) {
+        signOutLocally()
+    }
+    
+    
 }
