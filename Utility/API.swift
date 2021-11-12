fc85b92f4ebca3c1517d33a0d4fc73bef04475be
HiveYuan <45333458+HiveYuan@users.noreply.github.com>
2021-11-12 15:13:59 -0600

    get my events by username

--- Utility/API.swift
+++ Utility/API.swift
@@ -11,7 +11,7 @@ import Amplify
 class API {
     public static func getAll(where: QueryPredicate? = nil, sort: QuerySortInput? = nil) -> [Post] {
         var posts: [Post] = []
-        Amplify.DataStore.query(Post.self) {
+        Amplify.DataStore.query(Post.self, where: `where`, sort: sort) {
             result in
             switch(result) {
             case .success(let items):
@@ -22,38 +22,5 @@ class API {
         }
         return posts
     }
-//
-//    static func createPost() {
-//        let time = Date()
-//        let formatter = DateFormatter()
-//        formatter.dateStyle = .short
-//        formatter.timeStyle = .medium
-//        let timeString = formatter.string(from: time)
-//
-//        let postTest = Post(author: "Hive", departureTime: timeString, source: "Kingsbury", destination: "WashU", transportation: "On Foot2", description: "Go to blow up the school", maxMembers: i, title: "Let's go to place \(i)", postTime: timeString)
-//        let postTest = Post()
-//
-//        Amplify.DataStore.save(postTest) { result in
-//            switch result {
-//            case .success:
-//                print("Post saved successfully!")
-//            case .failure(let error):
-//                print("Error saving post \(error)")
-//            }
-//        }
-//    }
-    
-//    static func loadPosts() -> [Post] {
-//        var posts : [Post] = []
-//        Amplify.DataStore.query(Post.self){
-//            switch $0 {
-//            case .success(let result):
-//                // result will be of type [Post]
-//                posts = result
-//            case .failure(let error):
-//                print("Error on query() for type Post - \(error.localizedDescription)")
-//            }
-//        }
-//        return posts
-//    }
+
 }
