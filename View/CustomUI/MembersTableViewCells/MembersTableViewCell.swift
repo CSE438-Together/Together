e6034e23efa2f626a3d390b76cdd7e85edd99699
bingxinliu <bingxin.liu@wustl.edu>
2021-12-02 21:30:40 -0600

    update

--- View/CustomUI/MembersTableViewCells/MembersTableViewCell.swift
+++ View/CustomUI/MembersTableViewCells/MembersTableViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 class MembersTableViewCell: UITableViewCell {
     
     @IBOutlet var memberView : UILabel!
+    @IBOutlet var memberAvatarView : UIImageView!
+    @IBOutlet var shadowView : UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,8 +30,21 @@ class MembersTableViewCell: UITableViewCell {
         return UINib(nibName: "MembersTableViewCell", bundle: nil)
     }
     
-    public func configure( with memberId : String) {
+    public func configure( with memberId : String, with memberAvatar : UIImage) {
         self.memberView.text = memberId
+        self.memberAvatarView.image = memberAvatar
+        self.memberAvatarView.layer.cornerRadius = 15
+        
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        
+        self.shadowView.layer.shadowColor = UIColor.gray.cgColor
+        self.shadowView.layer.shadowOffset = CGSize(width: 2, height: 3)
+        self.shadowView.layer.shadowOpacity = 0.8
+        self.shadowView.layer.masksToBounds = false
+        self.shadowView.layer.cornerRadius = 15
+        
+        self.shadowView.layer.zPosition = -1
         
     }
     
